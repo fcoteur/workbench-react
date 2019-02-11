fa3f3fb2b9bc82748870075973ab5a172564cfcc
Francis Coteur <fcoteur@hotmail.com>
2019-02-11 22:13:49 +0100

    add city selection to weather application

--- src/WeatherApp/WeaterApp.js
+++ src/WeatherApp/WeaterApp.js
@@ -25,37 +25,59 @@ export default class WeaterApp extends Component {
       data: null,
       isLoading: false,
       error: null,
+      city: 'Bilbao,es',
+      selection: '',
+      visible: false
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleVisible = this.toggleVisible.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    this.fetchForecast()
+  }
 
-    const urlWeather =  'https://api.openweathermap.org/data/2.5/forecast?q=Bilbao,es&units=metric&appid=' + process.env.REACT_APP_APIKEY
+  fetchForecast() {
+    const urlWeather =  'https://api.openweathermap.org/data/2.5/forecast?q=' + this.state.city +'&units=metric&appid=' + process.env.REACT_APP_APIKEY
    
     this.setState({ isLoading: true });
 
     axios.get(urlWeather)
       .then(result => {
         this.setState({data: result.data, isLoading: false})
-        console.log(result.data)
       })
       .catch(error => this.setState({
         error,
         isLoading: false
       }));
   }
+
+  handleChange(e) {
+    this.setState({selection: e.target.value})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({city: this.state.selection, visible: false},() => this.fetchForecast())
+  }
+
+  toggleVisible() {
+    this.setState({visible: !this.state.visible})
+  }
   
   render() {
     const { data, isLoading, error } = this.state;
 
-    if (error) {
+    /* if (error) {
       return <p>{error.message}</p>;
     }
 
     if (isLoading | !data) {
       return <p>Loading ...</p>;
-    }
+    } */
 
+    // select two forecasts per 24h and limit display to 4 days maximum
     if (data) {
       const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
       var forecastList = data.list.map((forecast,index) => {
@@ -74,7 +96,23 @@ export default class WeaterApp extends Component {
 
     return (
       <Box>
-        <span><strong>Weather Forecast in </strong>{data.city.name}</span><br/>
+        <span>
+          <strong>Weather Forecast in </strong>
+          <div style={{display: this.state.visible === false ? "none" : "inline"}} >
+            <form onSubmit={this.handleSubmit} style={{display: 'inline'}}>
+              <select value={this.state.selection} onChange={this.handleChange}>
+                <option value="London,gb">London</option>
+                <option value="Brussels,be">Brussels</option>
+                <option value="Bilbao,es">Bilbao</option>
+                <option value="Haarlem,nl">Haarlem</option>
+              </select>
+              <input type='submit' value={String.fromCharCode(0x21B5)} />
+            </form>
+          <button onClick={this.toggleVisible} style={{display: 'inline'}}>{String.fromCharCode(0x2A2F)}</button>
+        </div>
+        </span>
+        <span style={{display: this.state.visible === true ? "none" : "inline"}} onClick={this.toggleVisible}>{this.state.city}</span>
+        <br/>
         {forecastList}
       </Box>
     )
